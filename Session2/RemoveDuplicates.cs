c53e059415e5528a0d5bdbbe157c16b77cc7bd2a
Dipen Rana <dipen.rana@hotmail.com>
2017-12-04 21:13:29 -0800

    Update RemoveDuplicates.cs

--- Session2/RemoveDuplicates.cs
+++ Session2/RemoveDuplicates.cs
@@ -1,1 +1,23 @@
 
+public ListNode deleteDuplicates(ListNode A) {
+            
+            ListNode cur = A;
+            ListNode next = A.next;
+            while(next != null)
+            {
+                if(cur.val == next.val)
+                {
+                    cur.next = next.next;
+                    next = cur.next;
+                }
+                else
+                {
+                    cur = next;
+                    next = next.next;
+                }
+            }
+            
+            return A;
+            
+            
+    }
