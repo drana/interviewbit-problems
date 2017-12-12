665a91c952cc73e88c2133b66dad1002ad068096
Dipen Rana <dipen.rana@hotmail.com>
2017-12-11 20:15:50 -0800

    Create InsertPos.java

---
+++ Session3/BeforeSession/InsertPos.java
@@ -0,0 +1,10 @@
+   public static int searchInsert(ArrayList<Integer> A, int B) {
+        if(A == null) return 0;
+        if(B <= A.get(0)) return 0;
+        
+        for(int i = 0; i < A.size()-1; i++){
+            if(A.get(i) < B && B <= A.get(i + 1))
+                return i + 1;
+        }
+        return A.size();              
+    }
