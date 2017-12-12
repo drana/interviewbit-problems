9f191ca12933d2b7865d66b9d91895e7de402eaf
Dipen Rana <dipen.rana@hotmail.com>
2017-12-11 20:10:54 -0800

    Update BalancedTree.java

--- Session3/BeforeSession/BalancedTree.java
+++ Session3/BeforeSession/BalancedTree.java
@@ -1,23 +1,32 @@
-public class Balanced{
-    public int isBalanced(TreeNode a) {
-        if(a == null)
+/**
+ * Definition for binary tree
+ * class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+	public int isBalanced(TreeNode a) {
+	    
+	    if(a == null)
             return 1;
-        int leftDepth = 0;
-        int rightDepth = 0;
+        int leftHeight = 0;
+        int rightHeight = 0;
         if(a.left != null)
-            leftDepth = depth(a.left);
+            leftHeight = Height(a.left);
         if(a.right != null)
-            rightDepth = depth(a.right);
-        if(Math.abs(leftDepth - rightDepth) <= 1 && isBalanced(a.left) == 1 && isBalanced(a.right) == 1)
+            rightHeight = Height(a.right);
+        if(Math.abs(leftHeight - rightHeight) <= 1 && isBalanced(a.left) == 1 && isBalanced(a.right) == 1)
             return 1;
         return 0;
         
-    }
-    public int depth(TreeNode a){
-        if(a == null)
+	}
+	
+	public int Height(TreeNode A){
+        if(A == null)
             return 0;
-        return 1 + Math.max(depth(a.left), depth(a.right));
-    }
-    public static void main(String[] args){
+        return 1 + Math.max(Height(A.left), Height(A.right));
     }
 }
