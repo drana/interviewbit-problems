e6087ef1869b015440739399e1c9493dd55ed963
Dipen Rana <dipen.rana@hotmail.com>
2018-01-21 20:25:39 -0800

    Create ReverseBits.java

---
+++ Session4/ReverseBits.java
@@ -0,0 +1,14 @@
+public class Solution {
+	public long reverse(long a) {
+	    
+	    long result = 0;
+        int i = 31;
+        while(a > 0){
+            result += (a % 2) * Math.pow(2, i);
+            i--;                        
+            a = a/2;
+        }
+        return result;
+        
+	}
+}
